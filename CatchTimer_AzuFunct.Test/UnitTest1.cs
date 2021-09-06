119fd5065cece9c3f5d20045d680f0bd5ab008f2
cesar7895123 <cesar7895123@hotmail.com>
2021-09-05 19:45:36 -0500

    Project base, Class, AzureFunction, UniTest

---
+++ CatchTimer_AzuFunct.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace CatchTimer_AzuFunct.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
